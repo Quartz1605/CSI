7d66a83d331bc527df40afa8c008145dc518afb1
Quartz1605 <prathambarekal05@gmail.com>
2025-03-25 13:20:58 +0530

    Started the project and made the base page..

---
+++ regpage/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className='w-full h-screen bg-black flex items-center justify-center'>
+    
+    <div className='text-red-100'>Welcome to the Registration Page.</div>
+      
+    </div>
+  )
+}
+
+export default App
